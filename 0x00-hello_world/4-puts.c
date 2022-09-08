0370c752d1e27d88c981bdd048b48974444475cb
geowan <geoffreymwangi5378@gmail.com>
2022-09-08 22:49:27 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program uses puts to output
+ * Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[53];
+
+	strcpy(str, "Programming is like building a multilingual puzzle \n");
+	puts(str);
+
+	return (0);
+
+}
