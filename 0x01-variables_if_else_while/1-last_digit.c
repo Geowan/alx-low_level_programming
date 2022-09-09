df4ebfd187fa80a305ff14018f975a6d16abe4fe
geowan <geoffreymwangi5378@gmail.com>
2022-09-09 23:12:50 +0300

    Task 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable n
+ * and prints a string depending on some conditions
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if (n < 6 && n > 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,2 @@
 Script 0-positive_or_negative.c  assign a random number to the variable n each time it is executed.
-
+Script 1-last_digit.c assign a random number to the variable n each time it is executed and outputs a predefined string 
