be9c66ad984574319c6ffbaa9133ec055cc2d2d9
geowan <geoffreymwangi5378@gmail.com>
2022-09-10 04:02:23 +0300

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 98)
+	{
+		j = i + 1;
+
+		while (j <= 99)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i < 98)
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+
+			j++;
+
+		}
+
+		i++;
+
+	}
+
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -22,4 +22,4 @@ Script 100-print_comb3.c t prints all possible different combinations of two dig
 
 Script 101-print_comb4.c prints all possible different combinations of three digits.
 
-
+Script 102-print_comb5.c prints all possible combinations of two two-digit numbers.
