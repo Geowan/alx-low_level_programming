4394f62546828e33bb64cd013c102a313ff5dca1
geowan <geoffreymwangi5378@gmail.com>
2022-09-15 18:12:46 +0300

    draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: integer that determines the times a character
+ * _ should be printed
+ *
+ * Return: void (nothing)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -28,4 +28,5 @@ File 4-print_most_numbers.c prints the numbers, from 0 to 9 excluding 2 and 4, f
 
 File 5-more_numbers.c prints 10 times the numbers, from 0 to 14, followed by a new line
 
+File 6-print_line.c draws a straight line in the terminal.
 
