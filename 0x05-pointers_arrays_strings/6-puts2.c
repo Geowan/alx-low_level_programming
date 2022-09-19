51910d65564f474ea2a614423a064bb6378819dc
geowan <geoffreymwangi5378@gmail.com>
2022-09-20 02:03:00 +0300

    fixed betty styles

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Function that prints every other character of a string, starting 
+ * puts2 - Function that prints every other character of a string, starting
  * with the first character followed by a new line.
  *
  * @str: Input string
