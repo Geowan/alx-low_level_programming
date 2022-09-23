ca2d094b5b9e8fe2cd01d28d50ac018281c56451
geowan <geoffreymwangi5378@gmail.com>
2022-09-23 03:40:30 +0300

    fixed output

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string.
- *
+ * _strncpy - function that copies a string.
  * @dest: Destination string pointer
  * @src: Source string pointer
  * @n: input integer value
- *
  * Return: pointer to the result
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int srclen = 0, i = 0;
-	char *basedest = dest, *basesrc = src;
+	char *output = dest, *basesrc = src;
 
 	while (*src != '\0')
 	{
@@ -27,7 +25,7 @@ char *_strncpy(char *dest, char *src, int n)
 	src = basesrc;
 
 	for (; i < n; i++)
-		*basedest++ = *src++;
+		*dest++ = *src++;
 
-	return (dest);
+	return (output);
 }
