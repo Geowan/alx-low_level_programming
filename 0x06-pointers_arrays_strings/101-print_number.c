3c153a546559bd58e356a2426b335149f8b20068
geowan <geoffreymwangi5378@gmail.com>
2022-09-23 05:19:00 +0300

    refactoring to single function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,5 @@
 #include "main.h"
 
-/**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
 
 /**
  * print_number - prints integers enters as parameters using putchar
@@ -24,29 +8,28 @@ int base10(int n)
  */
 void print_number(int n)
 {
-	int power;
-
-	power = base10(8);
+	unsigned int m, k, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n *= -1;
+		_putchar(45);
+		m = n * -1;
 	}
-
-	if (n == 0)
-		_putchar('0');
-
 	else
 	{
-		while (n / power == 0)
-			power /= 10;
-
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
+		m = n;
 	}
+	k = m;
+	count = 1;
+
+	while (k > 9)
+	{
+		k /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+
 }
